5794fcb6f0d196246f0aaf844160c339dbc6b028
Platon Kniazev <56154074+slowpokalipsis@users.noreply.github.com>
2021-05-22 15:26:21 +1000

    robo_tech_1

---
+++ robot_1.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open ('data.txt', 'r') as inp:
+    txt = [i for i in inp.readlines()]
+l1 = [i for i in txt[0][1:-1].split('][')]
+l2 = []
+for j in l1:
+    l2.append(j[1:-1].split('), ('))
+l3 = []
+for k in l2:
+    l3.append([tuple(map(float, l.split(', '))) for l in k])
+
+def change_tuple(tup, val):
+    y = list(tup)
+    del y[2]
+    y.append(val)
+    return tuple(y)
+
+fin_l = []
+
+for big_l in range(len(l3)):
+    almost_fin_l = []
+    for sm_l in range(len(l3[big_l])):
+        if l3[big_l][sm_l][2] < 5:
+            print(l3[big_l][sm_l])
+            almost_fin_l.append(change_tuple(l3[big_l][sm_l], 5.0))
+        elif l3[big_l][sm_l][2] > 4000:
+            print(l3[big_l][sm_l])
+            almost_fin_l.append(change_tuple(l3[big_l][sm_l], 4000.0))
+        else:
+            almost_fin_l.append(l3[big_l][sm_l])
+    fin_l.extend(almost_fin_l)
+
+fin_pd = pd.DataFrame(fin_l, columns=['quality', 'angle', 'distance'])
+fin_pd['x'] = fin_pd['distance'] * np.cos(fin_pd['angle'])
+fin_pd['y'] = fin_pd['distance'] * np.sin(fin_pd['angle'])
+fig, ax = plt.subplots()
+ax.plot(fin_pd['x'], fin_pd['y'], marker='o', linestyle='')
+ax.figure.set_size_inches(20, 20)
+plt.savefig('laser__в2рая_жизнь_туалетной_бумаги.png')
+
+
